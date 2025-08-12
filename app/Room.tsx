e7c856d4ace90d27c672190ecfdca91c35716f61
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-08-12 20:06:53 +0530

    FEAT: liveblocks setup and initial config

---
+++ app/Room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_Vj8KWdDIszlTahKPxq8_LbF3emlHsnVEJ6qU7yKersGVXrn40-8ncB8o3oQ9Clg4"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
